9ebbba9210b9c3d5e7984ebfb42a1c0f57a9400e
William Zhang <70344800+xernphoton@users.noreply.github.com>
2021-06-30 14:28:13 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('test.txt', 'r') as f:
-    x = json.load(f)
-with open('test.json', 'w') as f:
-    y = {}
-    for (key, value) in x.items():
-        y[key] = value['name']
-    json.dump(y, f)
